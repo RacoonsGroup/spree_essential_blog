3adfd79504c5409c5cc5c49d48e2b2b7cbc884b9
Volodya Protasevich <ggg1321@gmail.com>
2012-12-23 23:21:36 +0400

    Remove some white-spaces

--- app/controllers/spree/blogs/admin/blogs_controller.rb
+++ app/controllers/spree/blogs/admin/blogs_controller.rb
@@ -3,13 +3,13 @@ class Spree::Blogs::Admin::BlogsController < Spree::Admin::ResourceController
   def show
     redirect_to admin_blogs_path
   end
-  
+
 private
-  
+
   def find_resource
     Spree::Blog.find_by_permalink!(params[:id])
-  end  
-    
+  end
+
   def collection
     params[:search] ||= {}
     params[:search][:meta_sort] ||= "name.asc"
